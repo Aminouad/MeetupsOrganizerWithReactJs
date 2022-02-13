4035a2c9f64bf75ac33c69f23f2991eb2634cda5
Amine Ouadrani <ouadrani12@gmail.com>
2022-02-13 04:35:54 +0100

    add POST request to firebase using fetch

--- src/Components/meetups/NewMeetupForm.js
+++ src/Components/meetups/NewMeetupForm.js
@@ -1,7 +1,7 @@
 import Card from "../ui/Card";
 import classes from './NewMeetupForm.module.css';
 import { useRef } from 'react';
-function NewMeetupForm(){
+function NewMeetupForm(props){
     
     const titleInputRef=useRef();
     const imageInputRef=useRef();
@@ -25,7 +25,7 @@ function NewMeetupForm(){
              description:enteredDescription,
 
          };
-         console.log(meetupData);
+         props.onAddMeetup(meetupData);
    }   
    
     return (
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,21 @@
 import NewMeetupForm from "../Components/meetups/NewMeetupForm";
 
 function NewMeetupsPage(){
+    function addMeetupHandler(meetupData){
+          fetch('https://react-projectmeetup-default-rtdb.firebaseio.com/meetups.json',{
+              method:'POST',
+              body:JSON.stringify(meetupData),
+              headers:{
+                  'Content-Type':'application/json'
+              }
+          }); 
+
+
+    }
     return(
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
         </section>
     );
 }
