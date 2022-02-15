46a8af07130bf03640cde654a86bdae5428e3aeb
Amine Ouadrani <ouadrani12@gmail.com>
2022-02-15 04:44:43 +0100

    final commit adding the navigation counter for favorites

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,8 +1,26 @@
+import {useContext} from 'react';
+import MeetupList from '../Components/meetups/MeetupList';
+
+import FavoritesContext from '../store/favorite-context';
+
 function FavoritesPage(){
+  
+    const favoritesCtx=useContext(FavoritesContext);
+
+    let content;
+    if(favoritesCtx.totalFavorites===0){
+        content= <p>You got no favorites yet.Start adding some ?</p>
+
+    }
+    else{
+          content=<MeetupList meetups={favoritesCtx.favorites} />
+
+    }
+
     return(
-        <div>
-            Favorites Page
-        </div>
+        <section>
+            {content}
+        </section>
     );
 }
 export default FavoritesPage;
